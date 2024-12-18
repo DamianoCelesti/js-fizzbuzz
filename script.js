ba2deff45530292de8ddd7e56b011a4a9e9ca32d
Damiano Celesti <damiano.celesti@gmail.com>
2024-12-18 15:26:27 +0100

    add ex

---
+++ script.js
@@ -0,0 +1,25 @@
+// Fare loop dei numeri da 1 a 100
+
+// Se i numeri sono diviibili sia per 3 che per 5 stampare FizzBuzz
+
+// Se sono divisibili per 3 stampare Fizz
+
+// Se sono divisibili per 5 stamaper Buzz
+
+
+
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    }
+
+    else if (i % 3 === 0) {
+        console.log('Fizz');
+    }
+    else if (i % 5 === 0) {
+        console.log('Buzz');
+    }
+
+}
+
